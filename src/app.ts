f3d9db80881b675cecb7de25214a338ed4155386
Robert Halter <roebi@halter.in>
2023-10-19 18:25:20 +0200

    use of env

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
-const { App } = require("@slack/bolt");
+const { App, LogLevel } = require("@slack/bolt");
+
+import './env';
 
 // bolt using typescript
 // see
@@ -11,17 +13,16 @@ const { App } = require("@slack/bolt");
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
+  appToken: process.env.SLACK_APP_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  logLevel: LogLevel.DEBUG,
   // TODO https://github.com/roebi/slack-to-ticket/issues/3 remove socketMode
   // https://www.npmjs.com/package/@slack/bolt
   // uses SocketMode
-  socketMode: true,
-  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true
 });
 
 // listening to events https://slack.dev/bolt-js/concepts#event-listening
-
-// TODO https://github.com/roebi/slack-to-ticket/issues/8 do not use generalChannelId - should answer in the current thread of the current message
-const generalChannelId = "C061NA516DS";
 
 // TODO https://github.com/roebi/slack-to-ticket/issues/1 reaction_added is not recognized
 // https://www.npmjs.com/package/@slack/bolt#listening-for-events
@@ -29,15 +30,15 @@ const generalChannelId = "C061NA516DS";
 // For example: https://my-slack-app.example.com/slack/events.
 // Otherwise, all incoming requests from Slack won't be handled.
 
-// When a member has added an emoji reaction to an item
+// When a member has added an :ticket: emoji reaction to an item
 // https://api.slack.com/events/reaction_added
 app.event("reaction_added", async ({ event, client, logger }) => {
-  // TODO https://github.com/roebi/slack-to-ticket/issues/2 use the ticket emoji to trigger the event - current all emoji trigger the event
+  // TODO https://github.com/roebi/slack-to-ticket/issues/2 use the :ticket: emoji to trigger the event - current all emoji trigger the event
   try {
     // TODO https://github.com/roebi/slack-to-ticket/issues/8 do not use generalChannelId - should answer in the current thread of the current message
     // Call chat.postMessage with the built-in client
     const result = await client.chat.postMessage({
-      channel: generalChannelId,
+      channel: event.item.channel,
       text: `Thank you, <@${event.user.id}>, emoji recognized. Ticket will be created. Just a moment please. Wait here for the link to the ticket ...`,
     });
     logger.info(result);

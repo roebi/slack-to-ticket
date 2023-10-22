87d6d448f3ceff563b928fadcfa996cf5d903502
Robert Halter <roebi@halter.in>
2023-10-22 12:56:03 +0200

    add all in dist dir

---
+++ dist/consts.d.ts
@@ -0,0 +1,3 @@
+export declare const SLACK_API_BASE_URL = "https://api.slack.com/";
+export declare const SLACK_HTML_BASE_URL = "https://slack.com/";
+export declare const APP_SLACK_BASE_URL = "https://app.slack.com";
---
+++ dist/env.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const path_1 = require("path");
+const dotenv_1 = require("dotenv");
+const pathToConfig = "../.env";
+(0, dotenv_1.config)({ path: (0, path_1.resolve)(__dirname, pathToConfig) });
+//# sourceMappingURL=env.js.map
